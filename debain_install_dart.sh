bc1d0f4944bc1be89ad30a89048e112f87fb9b06
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2023-12-11 21:06:27 +0900

    Add files via upload

---
+++ debain_install_dart.sh
@@ -0,0 +1,13 @@
+
+
+sudo snap install flutter --classic
+
+sudo apt-add-repository ppa:dartsim/ppa 
+
+sudo apt-get update -y
+
+sudo apt-get install libdart6-all-dev -y
+
+flutter 
+
+dart --version
