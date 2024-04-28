d0bd38303aba826a0ccbb8d50f3c2d535be127a6
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-04-28 15:53:45 +0900

    Update t.sh

--- 14.自己的linux命令统计分析/t.sh
+++ 14.自己的linux命令统计分析/t.sh
@@ -4,6 +4,8 @@
 # source ~/.bashrc
 
 
-echo "alias phistory=\"history | awk '{CMD[\$2]++;count++;} END { for (a in CMD) print CMD[a] \" \" CMD[a]/count*100 \"% \" a;}' |" >> ~/.bashrc
-echo "grep -v './' | column -c3 -s ' ' -t | sort -nr | nl | head -n10\"" >> ~/.bashrc
-source ~/.bashrc
+echo "alias phistory=\"history | awk '{CMD[\$2]++;count++;} END { for (a in CMD) print CMD[a] \" \" CMD[a]/count*100 \"% \" a;}' | grep -v './' | column -c3 -s ' ' -t | sort -nr | nl | head -n10\"" >> ~/.bashrc
+
+
+# source ~/.bashrc   # 如果您使用的是 Bash shell
+source ~/.zshrc    # 如果您使用的是 Zsh shell
